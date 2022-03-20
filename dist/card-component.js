a5a4c812b22b3fcda58f2b4cf7791cda05da372a
Daniel Liu <hnnk@qq.com>
2022-03-20 15:00:36 +0800

    gh-pages

---
+++ dist/card-component.js
@@ -0,0 +1,2 @@
+import{_ as t,o as n,c as o,t as s,d as r}from"./assets/vue_vendors.c31cf230.js";var c=`h1{color:#00f}
+`;const m={props:{msg:String},setup(e){return(i,p)=>(n(),o("h1",null,"Card Component"+s(e.msg),1))}};var a=t(m,[["styles",[c]]]);const l=r(a);typeof window!="undefined"&&window.customElements.define("card-comp",l);
